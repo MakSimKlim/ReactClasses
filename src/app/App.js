ec8f49b9196d1a464669970f8e6980baaeb777e4
MakSimKlim <83maxomus@mail.ru>
2024-12-06 08:44:10 +0500

    add Posts

--- src/app/App.js
+++ src/app/App.js
@@ -1,38 +1,56 @@
 import logo from '../logo.svg';
+import React from 'react'
 import Hello from '../hello/Hello';
 import Length from '../length/Length';
 import Form from '../form/Form';
 import Range from '../range/Range';
+import Posts from '../posts/Posts'
 
 
 import './App.css';
 
-function App() {
+//function App() {
+class App extends React.Component
+{
+  state =
+  {
+    posts:
+    [
+      {id:"1", name:"C++ Basics", title:"Процедурное программирование на языке C++"},
+      {id:"2", name:"C++ OOP", title:"Объектно-ориентированное программирование на языке C++"},
+      {id:"3", name:"Windows via C/C++", title:"Разработка Windows приложений на языке C++"},
+    ]
+  }
+    render()
+    {
+          return (
+          // <div className="App">
+          //   <header className="App-header">
+          //     <img src={logo} className="App-logo" alt="logo" />
+          //     <p>
+          //       Edit <code>src/App.js</code> and save to reload.
+          //     </p>
+          //     <a
+          //       className="App-link"
+          //       href="https://reactjs.org"
+          //       target="_blank"
+          //       rel="noopener noreferrer"
+          //     >
+          //       Learn React
+          //     </a>
+          //   </header>
+          // </div>
+          <>
+          {/* <Hello name="Max"/>
+          <Length />
+          <Form />
+          <Range /> */}
+          
+          <Posts posts = {this.state.posts}/>
 
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <>
-    <Hello name="Max"/>
-    <Length />
-    <Form />
-    <Range />
-    </>
-  );
+          </>
+        );
+      }
 }
 
 export default App;
---
+++ src/posts/Post.js
@@ -0,0 +1,12 @@
+function Post(props)
+{
+    return(
+        <div>
+            <fieldset>
+                <legend style={{fontWeight:"bold"}}>{props.name}</legend>
+                <p>{props.title}</p>
+            </fieldset>
+        </div>
+    )
+}
+export default Post;
